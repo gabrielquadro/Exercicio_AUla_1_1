11ffc761fc99ad7b377cc276d0c43235a4d277ad
gabrielquadro <61526044+gabrielquadro@users.noreply.github.com>
2020-03-11 19:39:13 -0300

    Ciclo de vida do app

--- app/src/main/java/com/unisc/exercicio_aula_1_1/MainActivity.java
+++ app/src/main/java/com/unisc/exercicio_aula_1_1/MainActivity.java
@@ -33,6 +33,33 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
+    @Override
+    protected void onStart() {
+        Log.d("texto","onStart");
+        super.onStart();
+    }
 
+    @Override
+    protected void onResume() {
+        Log.d("texto","onResume");
+        super.onResume();
+    }
 
+    @Override
+    protected void onPause() {
+        Log.d("texto","onPause");
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d("texto","onStop");
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d("texto","onDestroy");
+        super.onDestroy();
+    }
 }
